88e7a22d0b68945d7efe2faa53e83b4fd5f3b3c7
manikantapmk <yadalapurapu.manikanta93@gmail.com>
2024-10-19 13:12:47 +0530

    first commit first house

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import './App.css'
+
+import Contact from "./pages/Contact/Contact"
+import Home from "./pages/Home/Home"
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <Contact />
+    </>
+  )
+}
+export default App
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h1 text-center'>First House Home</div>
+  )
+}
+
+export default Home
